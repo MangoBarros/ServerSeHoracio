be70af41a9b94bc65cc955fad499ddd5b63886fd
Miguel <miguel99bm@gmail.com>
2020-03-20 12:51:10 +0000

    metodos do sv prontos falta atualizar lista de pontuacoes

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var socket = require("socket.io");
 var listaEspera = [];
+var listaPontuacoes = [];
+var pessoasAtivas = [];
 // App setup
 
 var app = express();
@@ -20,9 +22,26 @@ io.on("connection", function(socket) {
     socket.on("listaEspera", function(data) {
         console.log(data);
         listaEspera.push(data.username);
-        //io.sockets.emit("listaEspera", data);
         listaEspera.forEach(element => {
             console.log(element);
         });
+    });
+
+    socket.on("proximoJogo", function() {
+        console.log(listaEspera);
+        console.log("primeiro");
+        if (listaEspera.length < 2) {
+            console.log("segundo");
+            io.sockets.emit("jogofail", null);
+        } else {
+            pessoasAtivas = listaEspera.splice(0, 2);
+            console.log(pessoasAtivas[0], pessoasAtivas[1]);
+            io.sockets.emit("novoJogo", pessoasAtivas);
+        }
+    });
+
+    socket.on("fimDoJogo", function(data) {
+        listaPontuacoes[data[0].username] = [data[0].username, data[0].pontuacao, data[0].id];
+        listaPontuacoes[data[1].username] = [data[1].username, data[1].pontuacao, data[1].id];
     });
 });
