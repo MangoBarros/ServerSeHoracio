21401c59f80db652fb98b8edcc6d535d13068919
Miguel <miguel99bm@gmail.com>
2020-03-27 18:29:46 +0000

    to heroku

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var socket = require("socket.io");
 var listaEspera = [];
 var listaPontuacoes = [];
 var pessoasAtivas = [];
+var tresPrimeiros = [];
+
 // App setup
 
 var app = express();
@@ -18,13 +20,15 @@ var io = socket(server);
 
 io.on("connection", function(socket) {
     console.log("made socket connection", socket.id);
-
+    io.sockets.emit("novoListaEspera", listaEspera);
+    io.sockets.emit("inserePontuacoes", tresPrimeiros);
     socket.on("listaEspera", function(data) {
         console.log(data);
         listaEspera.push(data.username);
         listaEspera.forEach(element => {
             console.log(element);
         });
+        io.sockets.emit("novoListaEspera", listaEspera);
     });
 
     socket.on("proximoJogo", function() {
@@ -35,13 +39,22 @@ io.on("connection", function(socket) {
             io.sockets.emit("jogofail", null);
         } else {
             pessoasAtivas = listaEspera.splice(0, 2);
-            console.log(pessoasAtivas[0], pessoasAtivas[1]);
+            console.log(pessoasAtivas[0], pessoasAtivas[1], "Emitindo novos jogadores");
             io.sockets.emit("novoJogo", pessoasAtivas);
         }
     });
 
     socket.on("fimDoJogo", function(data) {
-        listaPontuacoes[data[0].username] = [data[0].username, data[0].pontuacao, data[0].id];
-        listaPontuacoes[data[1].username] = [data[1].username, data[1].pontuacao, data[1].id];
+        console.log(data);
+        listaPontuacoes.push([data[0][1], data[0][0]]);
+        listaPontuacoes.push([data[1][1], data[1][0]]);
+        listaPontuacoes.sort(function(a, b) {
+            return b[0] - a[0];
+        });
+        console.log(listaPontuacoes);
+        tresPrimeiros[0] = listaPontuacoes[0];
+        tresPrimeiros[1] = listaPontuacoes[1];
+        tresPrimeiros[2] = listaPontuacoes[2];
+        io.sockets.emit("inserePontuacoes", tresPrimeiros);
     });
 });
---
+++ public/assets/lista.js
@@ -0,0 +1,135 @@
+//Make connection
+
+const socket = io.connect("http://localhost:4000");
+
+//butao para submeter o nome de um jogador
+const btn = document.querySelector("#send");
+const name = document.querySelector("#name");
+
+//butao para ir buscar os proximos jogadores e mostrar os seus nomes numa lista
+var btnJ = document.querySelector("#fetchJogadores");
+const listJ = document.querySelector(".listaJ");
+const player1 = document.querySelector("#player1");
+const player2 = document.querySelector("#player2");
+const listaEspera = document.querySelector("#listaEspera");
+
+const mensagem = document.querySelector("#mensagem");
+
+//butao para fazer o fetch das pontuacoes
+const btnP = document.querySelector("#acabarJogo");
+const listP = document.querySelector("#listaP");
+
+//Label jogadores em falta
+const h3Jogadores = document.querySelector("#failJogadores");
+
+//emit events
+
+var session_id;
+// Get saved data from sessionStorage
+let data = sessionStorage.getItem("sessionId");
+console.log(data);
+if (data == null) {
+    session_id = null; //when we connect first time
+    socket.emit("start-session", { sessionId: session_id });
+} else {
+    session_id = data; //when we connect n times
+    socket.emit("start-session", { sessionId: session_id });
+}
+
+//click para adicionar jogador
+btn.addEventListener("click", function() {
+    console.log(name.value);
+    socket.emit("listaEspera", {
+        username: name.value
+    });
+});
+
+//click para ir para o proximo jogo "fetch dos proximos jogadores"
+btnJ.addEventListener("click", function() {
+    console.log(player1.textContent, player1.textContent == "Waiting For Player");
+    if (player1.textContent == "Waiting For Player" && player2.textContent == "Waiting For Player") {
+        console.log("primeiiro");
+        socket.emit("proximoJogo", function() {
+            io.sockets.emit("proximoJogo");
+        });
+    }
+});
+
+//Click jogo acabou
+btnP.addEventListener("click", function() {
+    console.log("jogo acabou");
+    finishGame();
+});
+
+socket.on("novoListaEspera", function(data) {
+    console.log("hello");
+    listaEspera.innerHTML = "";
+    data.forEach(element => {
+        linhaEspera = document.createElement("li");
+        linhaEspera.textContent = element;
+        listaEspera.appendChild(linhaEspera);
+    });
+});
+
+socket.on("set-session-acknowledgement", function(data) {
+    mensagem.textContent = "Notificacao";
+    sessionStorage.setItem("sessionId", data.sessionId);
+});
+
+//falha no jogo
+socket.on("jogofail", function() {
+    createError();
+});
+
+//Pedido de novo jogo
+socket.on("novoJogo", function(data) {
+    console.log("recebendo novos jogadores");
+    createlist(data);
+});
+
+//Pedido de pontuacoes
+socket.on("inserePontuacoes", function(data) {
+    createlistaP(data);
+});
+
+function createError() {
+    console.log("errou");
+    h3Jogadores.textContent = "Não Há jogadores suficientes";
+}
+
+function finishGame() {
+    const pontuacoes = [
+        [player1.textContent, 10],
+        [player2.textContent, 20]
+    ];
+    console.log(pontuacoes);
+    console.log("changing labels");
+    player1.textContent = "Waiting For Player";
+    player2.textContent = "Waiting For Player";
+    console.log("wow");
+    socket.emit("fimDoJogo", pontuacoes);
+}
+
+function createlist(data) {
+    //h3Jogadores.textContent = "";
+    console.log(data[0], data[1]);
+
+    console.log("segundo");
+    player1.textContent = data[0];
+    player2.textContent = data[1];
+}
+
+function createlistaP(data) {
+    listP.innerHTML = "";
+    if (data.length != 0) {
+        for (let i = 0; i < 2; i++) {
+            console.log("primeiro");
+            const line = document.createElement("li");
+            console.log("segundo");
+            console.log(data[i]);
+            line.textContent = data[i][1] + " : " + data[i][0];
+            listP.appendChild(line);
+            console.log("terceito");
+        }
+    }
+}
